996d832756c19cfec66d51d1df2f8c7512d7cffe
星翼 <853974536@qq.com>
2018-08-10 18:43:08 +0800

    BUG FX

---
+++ RayTracer/RayTracer/Dielectirc.fs
@@ -0,0 +1,32 @@
+﻿namespace Material
+open Globals
+open Hitable
+open Ray
+open Material.MaterialFuncs
+
+type Dielectirc(albedo:Vec3,refIdx:float) =
+    interface IMaterial with
+        member this.Scatter(ray:Ray,record:HitRecord) : (Ray option*Vec3) =
+            let reflect = lazy (Reflect ray.Direction record.Normal)
+            let (normal,niNo,cos) =
+                match Vec3.Dot(ray.Direction,record.Normal) > 0.0 with
+                | true -> (
+                            -record.Normal,
+                            refIdx,
+                            refIdx * Vec3.Dot(ray.DirectionNorm,record.Normal))
+                | false -> (
+                            record.Normal,
+                            1.0/refIdx,
+                            -Vec3.Dot(ray.DirectionNorm, record.Normal))
+
+            let (reflectProb,refracted) = match Refract ray.Direction normal niNo with
+                                            | Some(refracted) -> (Schlick cos refIdx,Some(refracted))
+                                            | None -> (1.0,None)
+
+            let rand = System.Random(int(record.Position.X * record.Position.Y * record.Position.Z * 999999.0))
+            let outRay = match rand.NextDouble() < reflectProb with
+                            | true -> Ray(record.Position, reflect.Force())
+                            | false -> Ray(record.Position,refracted.Value)
+                
+            (Some(outRay),albedo)
+
--- RayTracer/RayTracer/Material.fs
+++ RayTracer/RayTracer/Material.fs
@@ -17,3 +17,17 @@ module MaterialFuncs =
 
     let Reflect (vin:Vec3) (normal:Vec3) = 
         vin - 2.0 * Vec3.Dot(vin,normal) * normal
+
+    let Refract (vin:Vec3) (normal:Vec3) (niNo:float) : Vec3 option =
+        let uvin = vin.Normalized()
+        let dt = Vec3.Dot(uvin,normal)
+        let disc = 1.0 - niNo * niNo * (1.0 - dt * dt)
+        match disc with
+        | disc when disc > 0.0 ->
+            Some(niNo * (uvin - normal * dt) - normal * Math.Sqrt(disc))
+        | _ -> None
+
+    let Schlick (cos:float) (refIdx:float) =
+        let r = (1.0-refIdx) / (1.0+refIdx)
+        let r2 = r*r
+        r2 + (1.0-r2)*Math.Pow((1.0-cos),5.0)
